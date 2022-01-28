0fcf01425e1c98279d3b7ab9b4ffea9faacd34fa
Ryan Love <love_ryan@bah.com>
2022-01-28 16:55:50 -0500

    Adding files for installing winget on a server image

---
+++ InstallWingetCli/InstallWingetCli.ps1
@@ -0,0 +1,3 @@
+﻿Add-AppxProvisionedPackage -Online -PackagePath .\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle `
+    -DependencyPackagePath .\Microsoft.VCLibs.x64.14.00.Desktop.appx `
+    -LicensePath .\08d8788d59cf47ed9bf42c31e31f8efa_License1.xml -Verbose
